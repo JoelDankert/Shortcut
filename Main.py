ac3f6525bba0c0158934f678094eba3c1ecbf9ea
JoelDankert <106259863+JoelDankert@users.noreply.github.com>
2022-05-30 16:24:38 +0200

    minor changes

--- Main.py
+++ Main.py
@@ -9,16 +9,16 @@ word.extend(['joel.dankert@gmail.com','+4917660186268','Ukii#3647','Ukii','xXUki
 shortcut.extend(['e m # ','t e l # ','d c # ','s t e a m # ','e p i c # '])
 
 #abkürzungen
-word.extend(['sorry','nice try','good luck, have fun!','my bad','thank you','no problem','good job'])
+word.extend(['sorry!','nice try.','good luck, have fun!','my bad...','thank you!','no problem.','good job!'])
 shortcut.extend(['s r y # ','n t # ','g l h f # ','m b # ','t h x # ','n p # ','g j # '])
 
 #addresses
 word.extend(['Aschenreutestraße 1, 78591 Durchhausen','Sallancher Str. 5, 78549 Spaichingen','In d. Breite 21, 78591 Durchhausen'])
 shortcut.extend(['a d . h o m e # ','a d . s c h o o l # ','a d . f a d w # '])
 
-keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','ä','ö','ü','ß','1','2','3','4','5','6','7','8','9','0','.',',','#','space','+','-','tab','shift','ctrl','alt']
-keystell = ['a ', 'b ', 'c ', 'd ', 'e ', 'f ', 'g ', 'h ', 'i ', 'j ', 'k ', 'l ', 'm ', 'n ', 'o ', 'p ', 'q ', 'r ', 's ', 't ', 'u ', 'v ', 'w ', 'x ', 'y ','z ','ae','oe','ue','ss','1 ','2 ','3 ','4 ','5 ','6 ','7 ','8 ','9 ','0 ','. ',', ','# ','  ','+ ','- ','ta','sh','co','al']
-keyremove = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0]
+keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','ä','ö','ü','ß','1','2','3','4','5','6','7','8','9','0','.',',','#','space','+','-','tab','shift','ctrl','alt','enter']
+keystell = ['a ', 'b ', 'c ', 'd ', 'e ', 'f ', 'g ', 'h ', 'i ', 'j ', 'k ', 'l ', 'm ', 'n ', 'o ', 'p ', 'q ', 'r ', 's ', 't ', 'u ', 'v ', 'w ', 'x ', 'y ','z ','ae','oe','ue','ss','1 ','2 ','3 ','4 ','5 ','6 ','7 ','8 ','9 ','0 ','. ',', ','# ','  ','+ ','- ','ta','sh','co','al','en']
+keyremove = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1]
 
 
 #                                                                                                                                       
@@ -78,6 +78,8 @@ while(True):
     checkkeys()
     addstrokes()
     checkpatterns()
+
+    #print(recordedkeys)
 
     #keep at end
     updatelaststate()
